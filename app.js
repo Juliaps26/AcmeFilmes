38549ac13a4103c7382b15d66cfc8fd8d01a2ff9
JULIA PEREIRA SILVA <23111810@CFP127ALUNO.LOCAL>
2024-01-25 13:33:09 -0300

    terminado

---
+++ app.js
@@ -0,0 +1,10 @@
+// Import da biblioteca para criar a API
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// app get
+
+
+
+
---
+++ controller/funcao.js
@@ -0,0 +1,44 @@
+// Função importa filmes 
+
+// Importando o módulo 
+const descFilmes = require('../modulo/filmes.js')
+
+// Criando uma função 
+const getFilmes = function(){
+     // Criando array 
+     let arrayFilmes=[]
+     // Contador valor zero
+     let contador=0
+     let status = false
+
+     // Loop while
+     while(contador<descFilmes.filmes.length){
+
+        let JSONFilmes={}
+        JSONFilmes.nome=descFilmes.filmes[contador].nome
+        JSONFilmes.sinopse=descFilmes.filmes[contador].sinopse
+        JSONFilmes.duracao=descFilmes.filmes[contador].duracao
+        JSONFilmes.data_lancamento=descFilmes.filmes[contador].data_lancamento
+        JSONFilmes.data_relancameto=descFilmes.filmes[contador].data_relancameto
+        JSONFilmes.foto_capa=descFilmes.filmes[contador].foto_capa
+        JSONFilmes.valor_unitario=descFilmes.filmes[contador].valor_unitario
+
+        arrayFilmes.push(JSONFilmes)
+        contador++
+        status=true
+        
+     }
+     if(status)
+     // Retorno da Função
+     return arrayFilmes
+     else
+     return false
+     
+ }
+    
+console.log(getFilmes())
+
+
+
+
+
